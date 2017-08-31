d1493f2a614ff8111c7e43df8553dfac03ed68f8
949458344@qq.com <100380sky>
2017-08-31 11:40:02 +0800

    Initial commit

---
+++ redis.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/2/22 0022
+ * Time: 13:54
+ */
+$redis = new redis();
+$result = $redis->connect('127.0.0.1', 6379);
+var_dump($result); //结果：bool(true)
+$result = $redis->set('test',"11111111111");
+var_dump($result);    //结果：bool(true)
+$result = $redis->get('test');
+var_dump($result);   //结果：string(11) "11111111111"
+die;
+require "redis.class.php";
+$redis = new RedisCluster();
+$redis->set('title','华育国际');
+echo $redis->get('title');
+?>
